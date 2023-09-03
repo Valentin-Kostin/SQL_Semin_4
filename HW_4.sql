6313ac29d61060a94a8d5f9a8d6b5239329986b7
Валентин Витальевич Костин <113250265+Valentin-Kostin@users.noreply.github.com>
2023-09-03 18:43:53 +0300

    2 task+

--- HW_4.sql
+++ HW_4.sql
@@ -5,3 +5,9 @@ JOIN users ON likes.user_id = users.id
 JOIN profiles ON users.id = profiles.user_id WHERE birthday>'2011-09-03'
 GROUP BY users.id;
 
+/*Определить кто больше поставил лайков (всего): мужчины или женщины.*/
+SELECT COUNT(likes.user_id), profiles.gender
+FROM likes
+JOIN users ON likes.user_id = users.id
+JOIN profiles ON users.id = profiles.user_id 
+GROUP BY gender;
